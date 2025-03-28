ea014317a6679a934956bef032797d3417e241e3
Kenneth Lai <kennethlai.ny@gmail.com>
2025-03-27 21:35:52 -0400

    Remove unneeded boilerplate

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import reactLogo from "/react.svg";
+import viteLogo from "/vite.svg";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div>
+      <img src={viteLogo} className="logo" alt="Vite logo" />
+      <img src={reactLogo} className="logo react" alt="React logo" />
+    </div>
+  );
+}
+
+export default App;
