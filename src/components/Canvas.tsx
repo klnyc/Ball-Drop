867a4d41d381e1b1b3807729461aa52b5bf7cfe4
Kenneth Lai <kennethlai.ny@gmail.com>
2025-03-27 22:52:26 -0400

    Add handle mouse movement

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,10 +1,11 @@
 import "../styles/Canvas.css";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
-const basket = { x: 150, y: 350, width: 100, height: 20 };
+const initialbasket = { x: 150, y: 350, width: 100, height: 20 };
 
 const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [basket, setBasket] = useState(initialbasket);
 
   const initiateCanvas = () => {
     const canvas = canvasRef.current;
@@ -19,14 +20,6 @@ const Canvas = () => {
 
     context.fillStyle = "#242424";
     context.fillRect(0, 0, canvas.width, canvas.height);
-  };
-
-  const drawGame = () => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-
-    const context = canvas.getContext("2d");
-    if (!context) return;
 
     context.fillStyle = "blue";
     context.fillRect(basket.x, basket.y, basket.width, basket.height);
@@ -35,25 +28,23 @@ const Canvas = () => {
     context.beginPath();
     context.arc(Math.random() * 300, 0, 10, 0, Math.PI * 2);
     context.fill();
-  };
-
-  useEffect(initiateCanvas, []);
-  useEffect(drawGame, []);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
 
     const handleMouse = (event: MouseEvent): void => {
       const rectangle = canvas?.getBoundingClientRect();
       const mouseX = event.clientX - rectangle.left;
-      basket.x = mouseX - basket.width / 2;
+
+      setBasket((previousBasket) => ({
+        ...previousBasket,
+        x: mouseX - previousBasket.width / 2,
+      }));
     };
 
     canvas.addEventListener("mousemove", handleMouse);
 
     return () => document.removeEventListener("mousemove", handleMouse);
-  }, []);
+  };
+
+  useEffect(initiateCanvas, [basket]);
 
   return <canvas ref={canvasRef} />;
 };
