99c42658aff8afddbc92d637ad8dee0132b3337e
Kenneth Lai <kennethlai.ny@gmail.com>
2025-03-27 23:19:13 -0400

    Add ball movement

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,33 +1,24 @@
 import "../styles/Canvas.css";
 import { useRef, useEffect, useState } from "react";
 
-const initialbasket = { x: 150, y: 350, width: 100, height: 20 };
+const initialBasket = { x: 150, y: 350, width: 100, height: 20 };
+const initialBall = {
+  x: Math.random() * 300,
+  y: 0,
+  width: 30,
+  height: 30,
+  speed: 2,
+};
 
 const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const [basket, setBasket] = useState(initialbasket);
+  const [gameOver, setGameOver] = useState(false);
+  const [basket, setBasket] = useState(initialBasket);
+  const [ball, setBall] = useState(initialBall);
 
   const initiateCanvas = () => {
     const canvas = canvasRef.current;
     if (!canvas) return;
-
-    // sets canvas dimensions to its parent container
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-
-    const context = canvas.getContext("2d");
-    if (!context) return;
-
-    context.fillStyle = "#242424";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    context.fillStyle = "blue";
-    context.fillRect(basket.x, basket.y, basket.width, basket.height);
-
-    context.fillStyle = "red";
-    context.beginPath();
-    context.arc(Math.random() * 300, 0, 10, 0, Math.PI * 2);
-    context.fill();
 
     const handleMouse = (event: MouseEvent): void => {
       const rectangle = canvas?.getBoundingClientRect();
@@ -41,10 +32,63 @@ const Canvas = () => {
 
     canvas.addEventListener("mousemove", handleMouse);
 
+    // sets canvas dimensions to its parent container
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+
+    const context = canvas.getContext("2d");
+    if (!context) return;
+
+    const drawGame = () => {
+      context.fillStyle = "#242424";
+      context.fillRect(0, 0, canvas.width, canvas.height);
+
+      context.fillStyle = "blue";
+      context.fillRect(basket.x, basket.y, basket.width, basket.height);
+
+      context.fillStyle = "red";
+      context.beginPath();
+      context.arc(ball.x, ball.y, 10, 0, Math.PI * 2);
+      context.fill();
+    };
+
+    const resetBall = () => {
+      setBall(initialBall);
+    };
+
+    const gameLoop = () => {
+      if (gameOver) return;
+
+      setBall((previousBall) => ({
+        ...previousBall,
+        y: previousBall.y + previousBall.speed,
+      }));
+
+      // if ball hits the basket
+      if (
+        ball.y + ball.height >= basket.y &&
+        ball.x >= basket.x &&
+        ball.x <= basket.x + basket.width
+      ) {
+        resetBall();
+      }
+
+      // if ball hits the ground
+      if (ball.y > canvas.height) {
+        console.log("you lose");
+        setGameOver(true);
+      }
+console.log("drawings")
+      drawGame();
+      requestAnimationFrame(gameLoop);
+    };
+
+    gameLoop();
+
     return () => document.removeEventListener("mousemove", handleMouse);
   };
 
-  useEffect(initiateCanvas, [basket]);
+  useEffect(initiateCanvas, []);
 
   return <canvas ref={canvasRef} />;
 };
