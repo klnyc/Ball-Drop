27d3523173191698a4750c56710c59811ff9e213
Kenneth Lai <kennethlai.ny@gmail.com>
2025-04-02 20:50:00 -0400

    Use mode flag to determine production build in defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/", // public base path in production build (dist)
-});
+  base: mode === "production" ? "/Ball-Drop/" : "/",
+}));
