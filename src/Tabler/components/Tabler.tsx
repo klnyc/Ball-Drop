b80685e7c970493f2eca32d09dc82e4ee937b9c5
Kenneth Lai <kennethlai.ny@gmail.com>
2026-07-30 11:48:03 -0400

    Add Tabler styling

--- src/Tabler/components/Tabler.tsx
+++ src/Tabler/components/Tabler.tsx
@@ -1,35 +1,42 @@
 import { type ChangeEvent, FormEvent, useState } from "react";
 import BackToPlayboxButton from "../../common/components/BackToPlayboxButton";
 
-// can't delete
-
 const Tabler = () => {
-  const [rows, setRows] = useState<number>();
-  const [columns, setColumns] = useState<number>();
+  const [rows, setRows] = useState<number | string>("");
+  const [columns, setColumns] = useState<number | string>("");
   const [table, setTable] = useState<string[][]>([]);
+  const [submitError, setSubmitError] = useState<string>("");
 
   const rowsOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (!isNaN(Number(event.target.value))) {
-      setRows(Number(event.target.value));
+    const value = event.target.value;
+    if (value === "") {
+      setRows("");
+    } else if (!isNaN(Number(value))) {
+      setRows(Number(value));
     }
   };
 
   const columnsOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (!isNaN(Number(event.target.value))) {
-      setColumns(Number(event.target.value));
+    const value = event.target.value;
+    if (value === "") {
+      setColumns("");
+    } else if (!isNaN(Number(value))) {
+      setColumns(Number(value));
     }
   };
 
   const onSubmit = (event: FormEvent) => {
     event?.preventDefault();
 
-    if (!rows || !columns || rows < 1 || columns < 1) {
+    if (typeof rows === "string" || typeof columns === "string") {
+      setSubmitError("Number must be between 1 and 10");
       return;
     }
 
     const newRow = Array(columns).fill("");
     const newTable = Array(rows).fill(newRow);
     setTable(newTable);
+    setSubmitError("");
   };
 
   const onInputChange = (
@@ -49,51 +56,58 @@ const Tabler = () => {
       </div>
 
       <form className="tabler-form" onSubmit={onSubmit}>
-        <div>
-          Rows:
+        <div className="tabler-form-input">
+          <span className="tabler-form-input-label">Rows:</span>
           <input
             name="rows"
             type="number"
             value={rows}
             onChange={rowsOnChange}
             min={1}
-            max={100}
+            max={10}
           />
         </div>
-
-        <div>
-          Columns:
+        <div className="tabler-form-input">
+          <span className="tabler-form-input-label">Columns:</span>
           <input
             name="columns"
             type="number"
             value={columns}
             onChange={columnsOnChange}
             min={1}
-            max={100}
+            max={10}
           />
         </div>
 
-        <button type="submit">Create table</button>
+        <div className="tabler-error">{submitError}</div>
+
+        <button className="tabler-create-button" type="submit">
+          Create table
+        </button>
       </form>
 
       {table.length > 0 && (
         <div className="tabler-table">
-          {table.map((row, rowIndex) => {
-            return (
-              <tr key={rowIndex}>
-                {row.map((_, columnIndex) => (
-                  <td key={`${rowIndex}-${columnIndex}`}>
-                    <input
-                      value={table[rowIndex][columnIndex]}
-                      onChange={(event) =>
-                        onInputChange(event, rowIndex, columnIndex)
-                      }
-                    />
-                  </td>
-                ))}
-              </tr>
-            );
-          })}
+          <table>
+            <tbody className="tabler-table-body">
+              {table.map((row, rowIndex) => {
+                return (
+                  <tr key={rowIndex}>
+                    {row.map((_, columnIndex) => (
+                      <td key={`${rowIndex}-${columnIndex}`}>
+                        <input
+                          value={table[rowIndex][columnIndex]}
+                          onChange={(event) =>
+                            onInputChange(event, rowIndex, columnIndex)
+                          }
+                        />
+                      </td>
+                    ))}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       )}
     </div>
